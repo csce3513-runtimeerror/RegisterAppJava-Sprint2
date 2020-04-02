51851e4f6d592de226ebcc6552bea397df02a472
sicilyg <sicilyguo321@gmail.com>
2020-04-02 13:33:06 -0500

    created ProductSearchRouteController.java class

---
+++ src/main/java/edu/uark/registerapp/controllers/ProductSearchRouteController.java
@@ -0,0 +1,14 @@
+package edu.uark.registerapp.controllers;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/productSearch")
+
+public class ProductSearchRouteController extends BaseController {
+	
+	
+}
