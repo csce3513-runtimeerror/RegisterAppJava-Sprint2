1a9ee43ea1dd05f1bcdc52772bd5b48272860fb2
az007a <60051173+az007a@users.noreply.github.com>
2020-04-02 20:30:06 -0500

    first commit

---
+++ src/main/java/edu/uark/registerapp/commands/transaction/TransactionQuery.java
@@ -0,0 +1,20 @@
+package edu.uark.registerapp.commands.transaction;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.uark.registerapp.commands.ResultCommandInterface;
+import edu.uark.registerapp.commands.exceptions.NotFoundException;
+import edu.uark.registerapp.models.api.Employee;
+import edu.uark.registerapp.models.entities.EmployeeEntity;
+import edu.uark.registerapp.models.repositories.EmployeeRepository;
+
+@Service
+public class TransactionQuery {
+    @Override
+    //todo
+    //@Autowired
+    //todo
+}
