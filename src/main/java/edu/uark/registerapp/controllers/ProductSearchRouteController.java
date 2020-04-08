8f2ca6b0e30c7d00659d55fa7999e8f5ff6f0eaa
mvpanis <60119225+mvpanis@users.noreply.github.com>
2020-04-08 15:23:33 -0500

    updated productsearchroutecontroller

--- src/main/java/edu/uark/registerapp/controllers/ProductSearchRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/ProductSearchRouteController.java
@@ -4,11 +4,14 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import edu.uark.registerapp.commands.products.ProductsQuery;
+import edu.uark.registerapp.models.api.Product;
 
 @Controller
 @RequestMapping(value = "/productSearch")
 
 public class ProductSearchRouteController extends BaseController {
+    @RequestMapping(method = RequestMethod.GET)
 	
 	
 }
