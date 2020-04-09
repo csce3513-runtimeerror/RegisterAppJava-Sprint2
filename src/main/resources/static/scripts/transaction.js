7e74e29f490006611559bce80f25b01c9fdcf54a
raculber <rculbert5678@gmail>
2020-04-09 15:00:44 -0500

    transaction html

---
+++ src/main/resources/static/scripts/transaction.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("checkout").addEventListener("click", checkout);
+});
+
+function checkout(event) {
+    alert("checkout");
+}
