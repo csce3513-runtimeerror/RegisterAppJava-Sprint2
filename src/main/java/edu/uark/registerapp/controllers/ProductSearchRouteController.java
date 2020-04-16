6ed3dea758f9d889b3593b84cc68dca1b7c1d173
dilen11 <dkhanald@gmail.com>
2020-04-16 16:18:46 -0500

    add some functionlatiy

--- src/main/java/edu/uark/registerapp/controllers/ProductSearchRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/ProductSearchRouteController.java
@@ -1,6 +1,6 @@
 package edu.uark.registerapp.controllers;
 
-import java.util.Map;
+//import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -9,7 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import edu.uark.registerapp.commands.products.ProductByLookupCodeQuery;
@@ -37,6 +37,8 @@ public class ProductSearchRouteController extends BaseController {
             modelAndView.addObject(ViewModelNames.PRODUCT.getValue(),productSearch.getLookupCode());
             return modelAndView;
             //NOT COMPLETE:: add whatever needed that I can't figure out at the current moment
+
+            
         }
         return new ModelAndView();
     }
