b01c26b3bd8f68a95c02b96b96bfb104f07a8a0c
sicilyg <sicilyguo321@gmail.com>
2020-04-27 13:14:44 -0500

    Finished ProductSearchRouteController

--- src/main/java/edu/uark/registerapp/controllers/ProductSearchRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/ProductSearchRouteController.java
@@ -1,6 +1,6 @@
 package edu.uark.registerapp.controllers;
 
-//import java.util.Map;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -9,44 +9,66 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import edu.uark.registerapp.commands.products.ProductByLookupCodeQuery;
+import edu.uark.registerapp.commands.exceptions.NotFoundException;
 import edu.uark.registerapp.commands.products.ProductByPartialSearchQuery;
-import edu.uark.registerapp.commands.products.ProductSearchCommand;
 import edu.uark.registerapp.controllers.enums.ViewModelNames;
 import edu.uark.registerapp.controllers.enums.ViewNames;
 import edu.uark.registerapp.models.api.ProductSearch;
 
-
 @Controller
 @RequestMapping(value = "/productSearch")
+public class ProductSearchRouteController extends BaseRouteController {
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView showSearch(
+        @RequestParam final Map<String, String> queryParameters
+    ) {
+        try {
+            this.productByPartialSearchQuery.execute();
+        } catch (NotFoundException e) {
+            return new ModelAndView(REDIRECT_PREPEND.concat(
+                ViewNames.PRODUCT_LISTING.getRoute()));
+        }
 
-public class ProductSearchRouteController extends BaseController {
+        ModelAndView modelAndView = this.setErrorMessageFromQueryString(
+            new ModelAndView(ViewNames.PRODUCT_SEARCH.getViewName()), 
+            queryParameters);
+
+        return modelAndView;
+    }
+
+
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public ModelAndView performProductSearch(ProductSearch productSearch,
     HttpServletRequest request) {
-        try {
-            this.productSearchCommand
+        /*try {
+            this.
                     .setSessionID(request.getSession().getId())
                     .setProductSearch(productSearch)
                     .execute();
         } catch (Exception e) {
-            ModelAndView modelAndView = new ModelAndView(ViewNames.PRODUCT_SEARCH.getViewName());
-            modelAndView.addObject(ViewModelNames.PRODUCT.getValue(),productSearch.getLookupCode());
-            return modelAndView;
-            //NOT COMPLETE:: add whatever needed that I can't figure out at the current moment
+            ModelAndView modelAndView = new ModelAndView(
+                ViewNames.PRODUCT_SEARCH.getViewName()
+            );
 
-            
-        }
-        return new ModelAndView();
+            modelAndView.addObject(ViewModelNames.ERROR_MESSAGE.getValue(),
+            e.getMessage());
+            modelAndView.addObject( 
+                ViewModelNames.PRODUCT_SEARCH.getValue(),
+                productSearch.getLookupCode()
+            );
+
+            return modelAndView;
+        }*/
+        return new ModelAndView(REDIRECT_PREPEND.concat(
+            ViewNames.PRODUCT_LISTING.getRoute())
+        );
     }
-	
-    @Autowired
-    private ProductSearchCommand productSearchCommand;
-    @Autowired
-    private ProductByPartialSearchQuery productByPartialSearchQuery;
+    
+    
+    //Properties
     @Autowired
     private ProductByPartialSearchQuery productByPartialSearchQuery;
 }
