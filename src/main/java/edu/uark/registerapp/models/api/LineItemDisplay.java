8f481bdfbe9d008855797f710c4a85a1320e4fcd
mvpanis <60119225+mvpanis@users.noreply.github.com>
2020-04-30 17:04:18 -0500

    added gets and sets

--- src/main/java/edu/uark/registerapp/models/api/LineItemDisplay.java
+++ src/main/java/edu/uark/registerapp/models/api/LineItemDisplay.java
@@ -1,14 +1,39 @@
 package edu.uark.registerapp.models.api;
 
 public class LineItemDisplay extends ApiResponse {
-     private String lookupCode;
-     private double price;
-     private double quantity;
-     public LineItemDisplay(String lookupCode, double price, double quantity){
+    private String lookupCode;
+    private double price;
+    private double quantity;
+
+    public LineItemDisplay(String lookupCode, double price, double quantity) {
         super();
-        this.lookupCode = lookupCode;
-        this.price = price;
+        this.setLookupCode(lookupCode);
+        this.setPrice(price);
+        this.setQuantity(quantity);
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
         this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getLookupCode() {
+        return lookupCode;
+    }
+
+    public void setLookupCode(String lookupCode) {
+        this.lookupCode = lookupCode;
     }
     
 }
