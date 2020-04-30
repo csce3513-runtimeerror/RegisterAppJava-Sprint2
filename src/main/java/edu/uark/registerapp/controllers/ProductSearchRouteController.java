e99ac1e518797031ef9a7aa220e17d8ca3c9d674
sicilyg <sicilyguo321@gmail.com>
2020-04-29 21:13:27 -0500

    Fingers crossed

--- src/main/java/edu/uark/registerapp/controllers/ProductSearchRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/ProductSearchRouteController.java
@@ -24,11 +24,8 @@ import edu.uark.registerapp.models.api.ProductSearch;
 @Controller
 @RequestMapping(value = "/productSearch")
 public class ProductSearchRouteController extends BaseRouteController {
-<<<<<<< HEAD
     @RequestMapping(value = "/{transactionId}", method = RequestMethod.GET)
-=======
-    @RequestMapping(value = "/{transactionId}")
->>>>>>> 1222549d38cab85b82a427c2528aa19ae2b99104
+    //@RequestMapping(value = "/{transactionId}")
     public ModelAndView showSearch(
         @RequestParam final UUID transactionId,
         @RequestParam final Map<String, String> queryParameters
